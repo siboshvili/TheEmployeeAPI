397199dc5990c3e035651e1f1651194cb5511bbb
siboshvili <siboshvili.andro@gmail.com>
2025-01-29 20:01:10 +0400

    Adding Logging to Controller.

---
+++ Extensions.cs
@@ -0,0 +1,21 @@
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace TheEmployeeAPI;
+
+public static class Extensions
+{
+
+    public static ModelStateDictionary ToModelStateDictionary(this ValidationResult validationResult)
+    {
+        var modelState = new ModelStateDictionary();
+
+        foreach (var error in validationResult.Errors)
+        {
+            modelState.AddModelError(error.PropertyName, error.ErrorMessage);
+        }
+
+        return modelState;
+    }
+
+}
